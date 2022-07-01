dd39e4fe36e8f28539632e3dade47c2efbe0751a
Ohanronnie <96521591+Ohanronnie@users.noreply.github.com>
2022-07-01 19:21:11 +0100

    Update note.js

--- note.js
+++ note.js
@@ -1,6 +1,23 @@
 let title = document.querySelector(".addtitle .title");
 let text = document.querySelector(".addcont .content");
+let date = new Date();
+let min = date.getHours();
+let hrs = date.getMinutes();
+let aec = date.getSeconds();
+let day = date.getDay();
+day++;
+if(day == 1){day="Sunday"}
+else if(day == 2){day="Mon"}
+else if(day == 3){day="Tues"}
+else if(day == 4){day="Wed"}
+else if(day == 5){day="Thurs"}
+else if(day == 6){day="Fri"}
+else if(day == 7){day="Sat"}
+    hrs = hrs < 10 ? "0" + hrs: hrs;
+   min = min < 10 ? "0" + min : min;
+    aec = aec < 10 ? "0" + aec : aec;
 
+let datee = day + '&nbsp' + min + ':' + hrs + ':' + aec;
 let savedNotes = localStorage.getItem("savedNotes2");
 
 if (savedNotes == null) notes = [];
@@ -14,7 +31,8 @@ else notes = JSON.parse(savedNotes);
 function addNote(){
     notes.push({
       note_title: title.value,
-      note_content: text.value
+      note_content: text.value,
+      note_time: datee
     });
 
     localStorage.setItem("savedNotes2", JSON.stringify(notes))
